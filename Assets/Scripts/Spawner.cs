f7fe32568dc03d58cb69ae26774e9508ec253f1e
NukaCrab <laurence.h.bennett@gmail.com>
2017-03-27 16:59:12 +0100

    PreFab and Spawner Start

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+        GameObject instance = (GameObject)Instantiate(Resources.Load("Zombie"));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
